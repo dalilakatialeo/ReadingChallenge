367628cb4b3c0355aaca7a6e6e4d07a03049acb0
Dalila K Leo <dalilakatialeo@gmail.com>
2022-01-25 22:46:50 +1000

    django setup and user model creation

--- readingchallenge/readingchallenge/settings.py
+++ readingchallenge/readingchallenge/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'users.apps.UsersConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -38,6 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 ]
+
+AUTH_USER_MODEL = 'users.CustomUser'
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
