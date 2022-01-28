a3ed8648437c2d8bf8a1ae94cc4d94230d6aa606
dkl <79177865+dalilakatialeo@users.noreply.github.com>
2022-01-29 07:47:36 +1000

    Update urls.py

--- readingchallenge/readingchallenge/urls.py
+++ readingchallenge/readingchallenge/urls.py
@@ -18,7 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('users/', include ('django.contrib.auth.urls')),
     path('users/', include ('users.urls')),
     path('challenges/', include ('challenges.urls'))   
 ]
