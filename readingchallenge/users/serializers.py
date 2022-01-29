d705ccd6273f37aa05c7d2e22c5d7b1b6aa5bc04
Dalila K Leo <dalilakatialeo@gmail.com>
2022-01-29 10:48:55 +1000

    user and admin token auth

--- readingchallenge/users/serializers.py
+++ readingchallenge/users/serializers.py
@@ -6,6 +6,7 @@ from .models import CustomUser
 class CustomUserSerializer(serializers.ModelSerializer):
     class Meta:
         model = CustomUser
+        
         fields = ("first_name", "last_name", "username", "email", "password")
         extra_kwargs = {"password": {"write_only": True}}
 
@@ -23,8 +24,9 @@ class CustomUserSerializer(serializers.ModelSerializer):
 #update user details - name and email
 
     def update(self, instance, validated_data):
+
         instance.first_name = validated_data.get('fist_name', instance.first_name)
-        instance.last_name = validated_data.get('first_name', instance.last_name)
+        instance.last_name = validated_data.get('last_name', instance.last_name)
         instance.username = validated_data.get('username', instance.username)
         instance.email = validated_data.get('email', instance.email)
         instance.password = validated_data.get('password', instance.password)
